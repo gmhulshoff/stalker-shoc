ebe0d311c26615a7f5010900a27968ba71e8a0ca
Gerard <gerard@ghulshoff.nl>
2022-09-25 15:06:37 +0200

    Added readJsonLtxSettings

--- webserver.js
+++ webserver.js
@@ -9,12 +9,14 @@ http
 	const params = url.parse(req.url, true).query
 	req.on('data', json => {
 		const body = JSON.parse(json)
-		if (req.url.startsWith('/readLtxSetting')) 
-			return res.end(readLtxSetting(body))
 		if (req.url.startsWith('/readAllLtxSections')) 
 			return res.end(readAllLtxSections(body))
+		if (req.url.startsWith('/readJsonLtxSections')) 
+			return res.end(readJsonLtxSections(body))
 		if (req.url.startsWith('/readLtxSectionSetting')) 
 			return res.end(readLtxSectionSetting(body))
+		if (req.url.startsWith('/updateLtxSectionSetting')) 
+			return res.end(updateLtxSectionSetting(body))
 	})
     req.on('end', () => {})
     console.log(req.url.substr(1))
@@ -38,34 +40,56 @@ http
   .listen(port)
 console.log(`Server running at http://127.0.0.1:${port}/`)
 
-function readLtxSetting(params, content = readFile(params)) {
-  const src = `.*${params.setting}\\s*?=(\\s)(?<${params.setting}>\\S*)\\s*?`
-  return content.match(new RegExp(src)).groups[params.setting]
-}
-
 function readAllLtxSections(params, content = readFile(params)) {
   const lines = content.split('\r\n')
-  console.log(lines.length)
-  const sections = []
-  return lines
-    .filter(line => line.startsWith('[') && line.endsWith(']'))
-	.map(line => line.substring(1, line.length - 1))
-	.slice(1)
-	.join('\r\n')
+  const sections = lines.slice(1).reduce((a,c) => {
+    if (c.startsWith('[') && c.endsWith(']')) {
+      a.current = c.substring(1, c.length - 1)
+	  a[a.current] = {}
+    }
+    const kvp = c.split('=').map(kvp => kvp.trim())
+	if (kvp.length == 2) a[a.current][kvp[0]] = kvp[1]
+	return a
+  }, {})
+  delete sections.current
+  return Object.keys(sections).filter(key => sections[key].type != 'storyline')
+    .join('\r\n')
+}
+
+function readJsonLtxSections(params, content = readFile(params)) {
+  const lines = content.split('\r\n')
+  const sections = lines.slice(1).reduce((a,c) => {
+    if (c.startsWith('[') && c.endsWith(']')) {
+      a.current = c.substring(1, c.length - 1)
+	  a[a.current] = {}
+    }
+    const kvp = c.split('=').map(kvp => kvp.trim())
+	if (kvp.length == 2) a[a.current][kvp[0]] = kvp[1]
+	return a
+  }, {})
+  delete sections.current
+  return JSON.stringify(sections)
 }
 
 function readLtxSectionSetting(params, content = readFile(params)) {
   const sectionSrc = `.*\\[${params.section}\\](?<${params.section}>[\\s\\S]*)(\\[|$)`
   const match = content.match(new RegExp(sectionSrc))
   const section = match.groups[params.section]
-  const texts = fs.readFileSync(`${params.path}/${params.texts}`).toString()
+  const texts = params.texts ? fs.readFileSync(`${params.path}/${params.texts}`).toString() : ''
   return params.setting.split(',').map(setting => {
-    const val = section.match(new RegExp(`.*${setting}\\s*?=(\\s)(?<${setting}>.*?\\r\\n)\\s*?`))
+    const val = section.match(new RegExp(`${setting}\\s*?=(\\s)(?<${setting}>.*?\\r\\n)\\s*?`))
 	  ?.groups[setting].trim()
-	if (['description','text'].includes(setting)) 
+	if (texts && ['description','text'].includes(setting)) 
       return readText(val, texts)?.split(',').join('').split('\n').join(' ')
 	return val
   }).join('\r\n')
+}
+
+function updateLtxSectionSetting(params, content = readFile(params)) {
+  const regexp = new RegExp(`${params.setting}\t\t=.*?${params.oldValue}`)
+  const newLine = `${params.setting}\t\t= ${params.newValue}`
+  writeFile(params, content.replace(regexp, newLine))
+  return `Updated ${params.path}/${params.fileName} with ${newLine}`
 }
 
 function readText(id, content) {
@@ -74,9 +98,8 @@ function readText(id, content) {
   return content.match(new RegExp(src))?.groups[id]
 }
 
-function readFile(params) {
-  return fs.readFileSync(`${params.path}/${params.fileName}`).toString()
-}
+function readFile(params) { return fs.readFileSync(`${params.path}/${params.fileName}`).toString() }
+function writeFile(params, content) { return fs.writeFileSync(`${params.path}/${params.fileName}`, content) }
 
 function getMimeTypes(extName) {
 	return {
